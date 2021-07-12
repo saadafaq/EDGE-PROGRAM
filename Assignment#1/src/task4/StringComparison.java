7320cc9c0ea3db718ae6e431f5ae5bfbb15aad49
saadafaq <48480533+saadafaq@users.noreply.github.com>
2021-07-12 14:34:01 +0500

    Add files via upload
    Code refactoring.

--- Assignment#1/src/task4/StringComparison.java
+++ Assignment#1/src/task4/StringComparison.java
@@ -2,6 +2,7 @@ package task4;
 /*
  * @Author : SAAD AHMED
  * @Description : Implement an algorithm to determine first common character in two character arrays.
+ * 
  * Constraints : 
  * Time complexity for this code O(m+n).
  * The space complexity should be O(1), and try within 40 bits
@@ -9,14 +10,14 @@ package task4;
  * No existing comparison functions are allowed.
  * 
  *  HINT: use bit vector (int for this task)
- */
 
+ */
 public class StringComparison {
 	public static void printFirstCommonCharacter(String str, String str2) {
 		 int checker = 0;
+		 
 		 for (int i = 0; i < str.length(); i++) {
-			 int val = str.charAt(i) - 'a';
-			 int bitShifting = 1 << val;
+			 int bitShifting = 1 << (str.charAt(i) - 'a');
 			 if ((checker & bitShifting) > 0) {
 				 continue;
 			 }
@@ -24,14 +25,13 @@ public class StringComparison {
 		 }
 		 
 		 for (int i = 0; i < str2.length(); i++) {
-			 int val = str2.charAt(i) - 'a';
-			 int bitShifting = 1 << val;
-			 if ((checker &  (1 << val)) > 0) {
+			 int bitShifting = 1 << (str2.charAt(i) - 'a');
+			 if ((checker & bitShifting ) > 0) {
 				 System.out.println( str2.charAt(i) + " is first common character.");
 				 break;
 			 }
 			 checker |= bitShifting;
-			 }
+		}
 		
 	}
 }
--- Assignment#1/src/task4/TestStringComparison.java
+++ Assignment#1/src/task4/TestStringComparison.java
@@ -1,8 +1,19 @@
 package task4;
-
+/*
+ * @Author : SAAD AHMED
+ * @Description : Implement an algorithm to determine first common character in two character arrays.
+ * 
+ * Constraints : 
+ * Time complexity for this code O(m+n).
+ * The space complexity should be O(1), and try within 40 bits
+ * The character array can only hold the lowercase letters a through z. 
+ * No existing comparison functions are allowed.
+ * 
+ *  HINT: use bit vector (int for this task)
+ */
 public class TestStringComparison {
 	public static void main(String[] args) {
-//		StringComparison.printFirstCommonCharacter("bca", "aef");
+		StringComparison.printFirstCommonCharacter("bca", "aef");
 		StringComparison.printFirstCommonCharacter("ab", "bc");
 
 	}
