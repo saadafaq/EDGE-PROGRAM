6a9a90e34cc7167af3aad2895d7c0f321c340b91
saadafaq <48480533+saadafaq@users.noreply.github.com>
2021-08-31 12:31:19 +0500

    Add files via upload
    Corrections.

--- React/Assignment-1/src/Sort.js
+++ React/Assignment-1/src/Sort.js
@@ -17,8 +17,6 @@ export class Sort{
 
 
     printArray(...array){
-        for(let i = 0 ; i < array.length ; i++){
-            console.log(array[i]);
-        }
+        console.log(array);
     }
 }
