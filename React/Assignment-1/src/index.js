99f3cccb9e68249f6a279cb5b5bf08d08aa4177e
saadafaq <48480533+saadafaq@users.noreply.github.com>
2021-08-31 12:32:47 +0500

    Add files via upload

--- React/Assignment-1/src/index.js
+++ React/Assignment-1/src/index.js
@@ -1,7 +1,7 @@
 import {Sort} from "./Sort";
 import User from "./model/User";
 
-var array = [5,2,6,10,23];
+var array = [5,2,6,24,10];
 
 var sort = new Sort();
 sort.sort(array);
@@ -11,4 +11,4 @@ var user = new User("Saad",24);
 
 const {name,age} = user; // It should be with the same name as the properties defined in your class
 
-console.log("name : "+name + ", age: "+age);
+console.log("[ name : "+name + ", age: "+age+" ]");
